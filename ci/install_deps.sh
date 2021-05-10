15a7f964b05e6d527c2d548630186b607af2162d
凌楚 <yangkun.ayk@alibaba-inc.com>
2021-05-10 13:59:50 +0800

    Fix typo

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -12,9 +12,9 @@ rm -rf "$GRPC_LOCAL_PATH" >/dev/null 2>&1
 docker cp dummy:/home/rocketmq/.m2/repository/io/grpc "$GRPC_LOCAL_PATH"
 
 # install protobuf to local repo.
-PROTOBUF_LATH_PATH="$HOME"/.m2/repository/com/google/protobuf/
+PROTOBUF_LOCAL_PATH="$HOME"/.m2/repository/com/google/protobuf/
 mkdir -p "$HOME"/.m2/repository/com/google
-rm -rf "$PROTOBUF_LATH_PATH" >/dev/null 2>&1
-docker cp dummy:/home/rocketmq/.m2/repository/com/google/protobuf "$PROTOBUF_LATH_PATH"
+rm -rf "$PROTOBUF_LOCAL_PATH" >/dev/null 2>&1
+docker cp dummy:/home/rocketmq/.m2/repository/com/google/protobuf "$PROTOBUF_LOCAL_PATH"
 
 docker rm -f dummy >/dev/null 2>&1
