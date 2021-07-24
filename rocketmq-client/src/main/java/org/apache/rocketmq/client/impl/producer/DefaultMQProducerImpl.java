92d066ad591178acb315b74f12fc7dcb4e7049e5
凌楚 <yangkun.ayk@alibaba-inc.com>
2021-07-24 14:36:41 +0800

    Polish code

--- rocketmq-client/src/main/java/org/apache/rocketmq/client/impl/producer/DefaultMQProducerImpl.java
+++ rocketmq-client/src/main/java/org/apache/rocketmq/client/impl/producer/DefaultMQProducerImpl.java
@@ -172,12 +172,11 @@ public class DefaultMQProducerImpl extends ClientBaseImpl {
     @Override
     public void doHealthCheck() {
         final Set<Endpoints> routeEndpointsSet = getRouteEndpointsSet();
-        final Set<Endpoints> diff = new HashSet<Endpoints>(Sets.difference(routeEndpointsSet,
-                                                                           isolatedRouteEndpointsSet));
+        final Set<Endpoints> aborted = new HashSet<Endpoints>(Sets.difference(routeEndpointsSet, isolatedRouteEndpointsSet));
         // Remove all isolated endpoints where is not in the topic route.
         isolatedRouteEndpointsSetLock.writeLock().lock();
         try {
-            isolatedRouteEndpointsSet.removeAll(diff);
+            isolatedRouteEndpointsSet.removeAll(aborted);
         } finally {
             isolatedRouteEndpointsSetLock.writeLock().unlock();
         }
