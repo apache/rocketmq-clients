a6b34ac909bfe699f8acc32e863bc3cd0e19fdab
凌楚 <yangkun.ayk@alibaba-inc.com>
2021-09-15 20:30:47 +0800

    Refactor AbstractOffsetStore

--- rocketmq-client/src/main/java/org/apache/rocketmq/client/impl/consumer/AbstractOffsetStore.java
+++ rocketmq-client/src/main/java/org/apache/rocketmq/client/impl/consumer/AbstractOffsetStore.java
@@ -21,23 +21,35 @@ import com.google.common.base.Optional;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import org.apache.rocketmq.client.message.MessageQueue;
+import org.apache.rocketmq.utility.ExecutorServices;
+import org.apache.rocketmq.utility.ThreadFactoryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Persist offset frequently is expensive in most case. Provide a simple way to persist offset for each
  * {@link MessageQueue}
  */
 public abstract class AbstractOffsetStore implements OffsetStore {
+    private static final Logger log = LoggerFactory.getLogger(AbstractOffsetStore.class);
+
     private long nanoTime;
     private final long persistPeriodSeconds;
 
     private final ConcurrentMap<MessageQueue, Long> offsetTable;
+    private final ScheduledExecutorService offsetPersistScheduler;
 
     public AbstractOffsetStore(long persistPeriodSeconds) {
         this.nanoTime = System.nanoTime();
         this.persistPeriodSeconds = persistPeriodSeconds;
         this.offsetTable = new ConcurrentHashMap<MessageQueue, Long>();
+        this.offsetPersistScheduler = new ScheduledThreadPoolExecutor(
+                1,
+                new ThreadFactoryImpl("OffsetPersistScheduler"));
     }
 
     @Override
@@ -45,6 +57,31 @@ public abstract class AbstractOffsetStore implements OffsetStore {
         final Map<MessageQueue, Long> queueOffsetTable = loadOffset();
         if (null != queueOffsetTable) {
             offsetTable.putAll(queueOffsetTable);
+        }
+        this.offsetPersistScheduler.scheduleWithFixedDelay(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            persistOffset(offsetTable);
+                        } catch (Throwable t) {
+                            log.error("Exception occurs while trying to persist offset", t);
+                        }
+                    }
+                },
+                persistPeriodSeconds,
+                persistPeriodSeconds,
+                TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void shutdown() {
+        try {
+            if (!ExecutorServices.awaitTerminated(offsetPersistScheduler)) {
+                log.error("[Bug] Timeout to shutdown the offset persist scheduler.");
+            }
+        } catch (Throwable t) {
+            log.error("Failed to shutdown the offset persist scheduler.", t);
         }
     }
 
