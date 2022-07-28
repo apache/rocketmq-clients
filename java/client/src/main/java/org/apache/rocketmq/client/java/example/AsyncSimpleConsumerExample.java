c3f668dd6f29e8698c9ba811c2528c7542feedae
Aaron Ai <yangkun.ayk@gmail.com>
2022-07-28 20:59:44 +0800

    Add client example

---
+++ java/client/src/main/java/org/apache/rocketmq/client/java/example/AsyncSimpleConsumerExample.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.rocketmq.client.java.example;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import org.apache.rocketmq.client.apis.ClientConfiguration;
+import org.apache.rocketmq.client.apis.ClientException;
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
+import org.apache.rocketmq.client.apis.SessionCredentialsProvider;
+import org.apache.rocketmq.client.apis.StaticSessionCredentialsProvider;
+import org.apache.rocketmq.client.apis.consumer.FilterExpression;
+import org.apache.rocketmq.client.apis.consumer.FilterExpressionType;
+import org.apache.rocketmq.client.apis.consumer.SimpleConsumer;
+import org.apache.rocketmq.client.apis.message.MessageId;
+import org.apache.rocketmq.client.apis.message.MessageView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AsyncSimpleConsumerExample {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AsyncSimpleConsumerExample.class);
+
+    private AsyncSimpleConsumerExample() {
+    }
+
+    public static void main(String[] args) throws ClientException, IOException {
+        final ClientServiceProvider provider = ClientServiceProvider.loadService();
+        String accessKey = "yourAccessKey";
+        String secretKey = "yourSecretKey";
+        SessionCredentialsProvider sessionCredentialsProvider =
+            new StaticSessionCredentialsProvider(accessKey, secretKey);
+        String endpoints = "foobar.com:8081";
+        ClientConfiguration clientConfiguration = ClientConfiguration.newBuilder()
+            .setEndpoints(endpoints)
+            .setCredentialProvider(sessionCredentialsProvider)
+            .build();
+        String consumerGroup = "yourConsumerGroup";
+        Duration awaitDuration = Duration.ofSeconds(30);
+        String tag = "yourMessageTagA";
+        String topic = "yourTopic";
+        FilterExpression filterExpression = new FilterExpression(tag, FilterExpressionType.TAG);
+        SimpleConsumer consumer = provider.newSimpleConsumerBuilder()
+            .setClientConfiguration(clientConfiguration)
+            // Set the consumer group name.
+            .setConsumerGroup(consumerGroup)
+            // set await duration for long-polling.
+            .setAwaitDuration(awaitDuration)
+            // Set the subscription for the consumer.
+            .setSubscriptionExpressions(Collections.singletonMap(topic, filterExpression))
+            .build();
+        // Max message num for each long polling.
+        int maxMessageNum = 16;
+        // Set message invisible duration after it is received.
+        Duration invisibleDuration = Duration.ofSeconds(30);
+        final CompletableFuture<List<MessageView>> future0 = consumer.receiveAsync(maxMessageNum, invisibleDuration);
+        future0.thenAccept(message -> {
+            final Map<MessageId, CompletableFuture<Void>> map =
+                message.stream().collect(Collectors.toMap(MessageView::getMessageId, consumer::ackAsync));
+            for (Map.Entry<MessageId, CompletableFuture<Void>> entry : map.entrySet()) {
+                final MessageId messageId = entry.getKey();
+                final CompletableFuture<Void> future = entry.getValue();
+                future.thenAccept(v -> LOGGER.info("Message is acknowledged successfully, messageId={}", messageId))
+                    .exceptionally(throwable -> {
+                        LOGGER.error("Message is failed to be acknowledged, messageId={}", messageId);
+                        return null;
+                    });
+            }
+        }).exceptionally(t -> {
+            LOGGER.error("Failed to receive message from remote", t);
+            return null;
+        });
+        // Close the simple consumer when you don't need it anymore.
+        consumer.close();
+    }
+}
