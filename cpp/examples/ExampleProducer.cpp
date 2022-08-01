6d5771e526add4a7e2c92734dbf8972920ac8a37
Li Zhanhui <lizhanhui@gmail.com>
2022-08-01 16:41:17 +0800

    Trigger CI build

--- cpp/examples/ExampleProducer.cpp
+++ cpp/examples/ExampleProducer.cpp
@@ -66,6 +66,7 @@ int main(int argc, char* argv[]) {
   logger.setLevel(Level::Info);
   logger.init();
 
+  // Access Key/Secret pair may be acquired from management console
   CredentialsProviderPtr credentials_provider;
   if (!FLAGS_access_key.empty() && !FLAGS_access_secret.empty()) {
     credentials_provider = std::make_shared<StaticCredentialsProvider>(FLAGS_access_key, FLAGS_access_secret);
