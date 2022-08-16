d7041c7281b1299e71094ecfaac6da70f9bdd79a
guyinyou <guyinyou@B-QD2CYVVG-0407.local>
2022-08-16 17:34:21 +0800

    fix bug in changeInvisibleDuration

--- java/client/src/main/java/org/apache/rocketmq/client/java/impl/consumer/ConsumerImpl.java
+++ java/client/src/main/java/org/apache/rocketmq/client/java/impl/consumer/ConsumerImpl.java
@@ -211,7 +211,7 @@ abstract class ConsumerImpl extends ClientImpl {
             @Override
             public void onFailure(Throwable t) {
                 final Duration duration = stopwatch.elapsed();
-                doAfter(MessageHookPoints.ACK, messageCommons, duration, MessageHookPointsStatus.ERROR);
+                doAfter(MessageHookPoints.CHANGE_INVISIBLE_DURATION, messageCommons, duration, MessageHookPointsStatus.ERROR);
                 LOGGER.error("Exception raised during message acknowledgement, messageId={}, endpoints={}, clientId={}",
                     messageId, endpoints, clientId, t);
 
