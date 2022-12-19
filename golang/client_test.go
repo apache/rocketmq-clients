707ef99259d4d6cf58a12e5d9607917110f748d2
guyinyou <guyinyou.gyy@alibaba-inc.com>
2022-12-19 14:34:36 +0800

    fix utest

--- golang/client_test.go
+++ golang/client_test.go
@@ -20,11 +20,46 @@ package golang
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/apache/rocketmq-clients/golang/credentials"
+	gomock "github.com/golang/mock/gomock"
+	"github.com/prashantv/gostub"
 )
 
 func TestCLINewClient(t *testing.T) {
+	stubs := gostub.Stub(&defaultClientManagerOptions, clientManagerOptions{
+		RPC_CLIENT_MAX_IDLE_DURATION: time.Second,
+
+		RPC_CLIENT_IDLE_CHECK_INITIAL_DELAY: time.Hour,
+		RPC_CLIENT_IDLE_CHECK_PERIOD:        time.Hour,
+
+		HEART_BEAT_INITIAL_DELAY: time.Hour,
+		HEART_BEAT_PERIOD:        time.Hour,
+
+		LOG_STATS_INITIAL_DELAY: time.Hour,
+		LOG_STATS_PERIOD:        time.Hour,
+
+		SYNC_SETTINGS_DELAY:  time.Hour,
+		SYNC_SETTINGS_PERIOD: time.Hour,
+	})
+
+	stubs2 := gostub.Stub(&NewRpcClient, func(target string, opts ...RpcClientOption) (RpcClient, error) {
+		if target == fakeAddresss {
+			return MOCK_RPC_CLIENT, nil
+		}
+		return nil, fmt.Errorf("invalid target=%s", target)
+	})
+
+	defer func() {
+		stubs.Reset()
+		stubs2.Reset()
+	}()
+
+	MOCK_RPC_CLIENT.EXPECT().Telemetry(gomock.Any()).Return(&MOCK_MessagingService_TelemetryClient{
+		trace: make([]string, 0),
+	}, nil)
+
 	endpoints := fmt.Sprintf("%s:%d", fakeHost, fakePort)
 	cli, err := NewClient(&Config{
 		Endpoint:    endpoints,
