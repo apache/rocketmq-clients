624dad3c904b7fdf2c9a04c411317119e506953f
SSpirits <admin@lv5.moe>
2023-04-04 19:56:31 +0800

    chore(proto): introduce rocketmq-apis as submodule

--- rust/build.rs
+++ rust/build.rs
@@ -21,10 +21,10 @@ fn main() {
         .out_dir("src/pb")
         .compile(
             &[
-                "proto/apache/rocketmq/v2/service.proto",
-                "proto/apache/rocketmq/v2/admin.proto",
+                "../rocketmq-apis/apache/rocketmq/v2/service.proto",
+                "../rocketmq-apis/apache/rocketmq/v2/admin.proto",
             ],
-            &["proto"],
+            &["../rocketmq-apis"],
         )
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
 }
