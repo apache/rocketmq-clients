cc81ffce87b130700d04cad6ac4974895dfaf777
yanchaomei <1653720237@qq.com>
2023-06-01 00:37:12 +0800

    add MessageIdCodec class

---
+++ python/client/message/MessageIdCodec.py
@@ -0,0 +1,69 @@
+import os
+import threading
+import time
+import uuid
+from datetime import datetime, timezone
+
+
+class MessageIdCodec:
+    MESSAGE_ID_LENGTH_FOR_V1_OR_LATER = 34
+    MESSAGE_ID_VERSION_V0 = "00"
+    MESSAGE_ID_VERSION_V1 = "01"
+
+    _instance = None
+    _lock = threading.Lock()
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            with cls._lock:
+                if not cls._instance:
+                    cls._instance = super(MessageIdCodec, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        self.processFixedStringV1 = self._get_process_fixed_string()
+        self.secondsSinceCustomEpoch = self._get_seconds_since_custom_epoch()
+        self.secondsStartTimestamp = int(time.time())
+        self.seconds = self._delta_seconds()
+        self.sequence = 0
+
+    def _get_process_fixed_string(self):
+        mac = uuid.getnode()
+        mac = format(mac, '012x')
+        mac_bytes = bytes.fromhex(mac[-12:])
+        pid = os.getpid()
+        pid_bytes = pid.to_bytes(2, 'big')
+        return mac_bytes.hex() + pid_bytes.hex()
+
+    def _get_seconds_since_custom_epoch(self):
+        custom_epoch = datetime(2021, 1, 1, tzinfo=timezone.utc)
+        now = datetime.now(timezone.utc)
+        return int((now - custom_epoch).total_seconds())
+
+    def _delta_seconds(self):
+        return int(time.time()) - self.secondsStartTimestamp \
+                    + self.secondsSinceCustomEpoch
+
+    def next_message_id(self):
+        self.sequence += 1
+        self.seconds = self._delta_seconds()
+        seconds_bytes = self.seconds.to_bytes(4, 'big')
+        sequence_bytes = self.sequence.to_bytes(4, 'big')
+        return (
+                self.MESSAGE_ID_VERSION_V1
+                + self.processFixedStringV1
+                + seconds_bytes.hex()
+                + sequence_bytes.hex()
+            )
+
+    def decode(self, message_id):
+        if len(message_id) != self.MESSAGE_ID_LENGTH_FOR_V1_OR_LATER:
+            return self.MESSAGE_ID_VERSION_V0, message_id
+        return message_id[:2], message_id[2:]
+
+
+if __name__ == "__main__":
+    codec = MessageIdCodec()
+    next_id = codec.next_message_id()
+    print(next_id)
+    print(codec.decode(next_id+'123'))
