6f4a605b72af67c4821024b0994a53f34bf7e9f0
yanchaomei <1653720237@qq.com>
2023-06-02 22:05:16 +0800

    fix style & license issues

--- python/rocketmq/rpc_client.py
+++ python/rocketmq/rpc_client.py
@@ -1,10 +1,26 @@
-import time
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import asyncio
-import certifi
-from rocketmq import logger
+import time
 from datetime import timedelta
+
+import certifi
 from grpc import aio, ssl_channel_credentials
 from protocol import service_pb2
+from rocketmq import logger
 from rocketmq.protocol import service_pb2_grpc
 
 
