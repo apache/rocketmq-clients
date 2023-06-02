1890a7eea5de72d15120fda4b4e414e75f62eac9
Aaron Ai <yangkun.ayk@gmail.com>
2023-06-02 16:29:12 +0800

    Format code

--- python/rocketmq/__init__.py
+++ python/rocketmq/__init__.py
@@ -4,14 +4,18 @@ import logging
 logger = logging.getLogger("rocketmqlogger")
 logger.setLevel(logging.DEBUG)
 
-log_path = os.path.join(os.path.expanduser("~"), "logs", "rocketmq", "rocketmq-client.log")
+log_path = os.path.join(
+    os.path.expanduser("~"), "logs", "rocketmq", "rocketmq-client.log"
+)
 file_handler = logging.FileHandler(log_path)
 file_handler.setLevel(logging.DEBUG)
 
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('%(asctime)s [%(levelname)s] [%(process)d] [%(filename)s#%(funcName)s:%(lineno)d] %(message)s')
+formatter = logging.Formatter(
+    "%(asctime)s [%(levelname)s] [%(process)d] [%(filename)s#%(funcName)s:%(lineno)d] %(message)s"
+)
 file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
 
--- python/rocketmq/rpc_client.py
+++ python/rocketmq/rpc_client.py
@@ -10,54 +10,84 @@ from rocketmq.protocol import service_pb2_grpc
 
 class RpcClient:
     channel_options = [
-        ('grpc.max_send_message_length', -1),
-        ('grpc.max_receive_message_length', -1),
-        ('grpc.connect_timeout_ms', 3000),
+        ("grpc.max_send_message_length", -1),
+        ("grpc.max_receive_message_length", -1),
+        ("grpc.connect_timeout_ms", 3000),
     ]
 
     def __init__(self, endpoints: str, ssl_enabled: bool = True):
         self.__endpoints = endpoints
         self.__cert = certifi.contents().encode(encoding="utf-8")
         if ssl_enabled:
-            self.__channel = aio.secure_channel(endpoints, ssl_channel_credentials(root_certificates=self.__cert),
-                                                options=RpcClient.channel_options)
+            self.__channel = aio.secure_channel(
+                endpoints,
+                ssl_channel_credentials(root_certificates=self.__cert),
+                options=RpcClient.channel_options,
+            )
         else:
-            self.__channel = aio.insecure_channel(endpoints, options=RpcClient.channel_options)
+            self.__channel = aio.insecure_channel(
+                endpoints, options=RpcClient.channel_options
+            )
         self.__stub = service_pb2_grpc.MessagingServiceStub(self.__channel)
         self.activity_nano_time = time.monotonic_ns()
 
     def idle_duration(self):
-        return timedelta(microseconds=(time.monotonic_ns() - self.activity_nano_time) / 1000)
+        return timedelta(
+            microseconds=(time.monotonic_ns() - self.activity_nano_time) / 1000
+        )
 
-    async def query_route(self, request: service_pb2.QueryRouteRequest, timeout_seconds: int):
+    async def query_route(
+        self, request: service_pb2.QueryRouteRequest, timeout_seconds: int
+    ):
         return await self.__stub.QueryRoute(request, timeout=timeout_seconds)
 
-    async def heartbeat(self, request: service_pb2.HeartbeatRequest, timeout_seconds: int):
+    async def heartbeat(
+        self, request: service_pb2.HeartbeatRequest, timeout_seconds: int
+    ):
         return await self.__stub.Heartbeat(request, timeout=timeout_seconds)
 
-    async def send_message(self, request: service_pb2.SendMessageRequest, timeout_seconds: int):
+    async def send_message(
+        self, request: service_pb2.SendMessageRequest, timeout_seconds: int
+    ):
         return await self.__stub.SendMessage(request, timeout=timeout_seconds)
 
-    async def query_assignment(self, request: service_pb2.QueryAssignmentRequest, timeout_seconds: int):
+    async def query_assignment(
+        self, request: service_pb2.QueryAssignmentRequest, timeout_seconds: int
+    ):
         return await self.__stub.QueryAssignment(request, timeout=timeout_seconds)
 
-    async def ack_message(self, request: service_pb2.AckMessageRequest, timeout_seconds: int):
+    async def ack_message(
+        self, request: service_pb2.AckMessageRequest, timeout_seconds: int
+    ):
         return await self.__stub.AckMessage(request, timeout=timeout_seconds)
 
-    async def forward_message_to_dead_letter_queue(self, request: service_pb2.ForwardMessageToDeadLetterQueueRequest,
-                                                   timeout_seconds: int):
-        return await self.__stub.ForwardMessageToDeadLetterQueue(request, timeout=timeout_seconds)
+    async def forward_message_to_dead_letter_queue(
+        self,
+        request: service_pb2.ForwardMessageToDeadLetterQueueRequest,
+        timeout_seconds: int,
+    ):
+        return await self.__stub.ForwardMessageToDeadLetterQueue(
+            request, timeout=timeout_seconds
+        )
 
-    async def end_transaction(self, request: service_pb2.EndTransactionRequest, timeout_seconds: int):
+    async def end_transaction(
+        self, request: service_pb2.EndTransactionRequest, timeout_seconds: int
+    ):
         return await self.__stub.EndTransaction(request, timeout=timeout_seconds)
 
-    async def notify_client_termination(self, request: service_pb2.NotifyClientTerminationRequest,
-                                        timeout_seconds: int):
-        return await self.__stub.NotifyClientTermination(request, timeout=timeout_seconds)
+    async def notify_client_termination(
+        self, request: service_pb2.NotifyClientTerminationRequest, timeout_seconds: int
+    ):
+        return await self.__stub.NotifyClientTermination(
+            request, timeout=timeout_seconds
+        )
 
-    async def change_invisible_duration(self, request: service_pb2.ChangeInvisibleDurationRequest,
-                                        timeout_seconds: int):
-        return await self.__stub.ChangeInvisibleDuration(request, timeout=timeout_seconds)
+    async def change_invisible_duration(
+        self, request: service_pb2.ChangeInvisibleDurationRequest, timeout_seconds: int
+    ):
+        return await self.__stub.ChangeInvisibleDuration(
+            request, timeout=timeout_seconds
+        )
 
 
 async def test():
