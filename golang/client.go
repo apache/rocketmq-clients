973c9580d4ed1b4cf31f9f7df1b290d22b50c6a0
guyinyou <36399867+guyinyou@users.noreply.github.com>
2023-09-05 10:56:49 +0800

    golang: optimize the logic of obtaining routing tasks (#580)
    * golang: optimize the logic of obtaining routing tasks
    
    * add log when queryRoute return nil
    
    ---------
    
    Co-authored-by: guyinyou <guyinyou.gyy@alibaba-inc.com>

--- golang/client.go
+++ golang/client.go
@@ -363,6 +363,7 @@ func (cli *defaultClient) queryRoute(ctx context.Context, topic string, duration
 	}
 
 	if len(response.GetMessageQueues()) == 0 {
+		cli.log.Errorf("queryRoute result has no messageQueue, requestId=%s", utils.GetRequestID(ctx))
 		return nil, errors.New("rocketmq: no available brokers")
 	}
 	return response.GetMessageQueues(), nil
@@ -499,7 +500,14 @@ func (cli *defaultClient) startUp() error {
 		cli.router.Range(func(k, v interface{}) bool {
 			topic := k.(string)
 			oldRoute := v
-			newRoute, _ := cli.queryRoute(context.TODO(), topic, cli.opts.timeout)
+			newRoute, err := cli.queryRoute(context.TODO(), topic, cli.opts.timeout)
+			if err != nil {
+				cli.log.Errorf("scheduled queryRoute err=%v", err)
+			}
+			if newRoute == nil && oldRoute != nil {
+				cli.log.Info("newRoute is nil, but oldRoute is not. do not update")
+				return true
+			}
 			if !reflect.DeepEqual(newRoute, oldRoute) {
 				cli.router.Store(k, newRoute)
 				switch impl := cli.clientImpl.(type) {
