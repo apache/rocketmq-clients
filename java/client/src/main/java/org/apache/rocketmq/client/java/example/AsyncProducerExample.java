4b7c41159548937efc89a3c3daa0bd786ff1ff4b
kaikoo <42601684+kingkh1995@users.noreply.github.com>
2023-11-06 10:08:46 +0800

    Update out-of-date java client examples (#627)
    * update out-of-date java client examples
    
    * Remove semaphore from AsyncSimpleConsumerExample
    
    Signed-off-by: Aaron Ai <yangkun.ayk@gmail.com>
    
    ---------
    
    Signed-off-by: Aaron Ai <yangkun.ayk@gmail.com>
    Co-authored-by: Aaron Ai <yangkun.ayk@gmail.com>

--- java/client/src/main/java/org/apache/rocketmq/client/java/example/AsyncProducerExample.java
+++ java/client/src/main/java/org/apache/rocketmq/client/java/example/AsyncProducerExample.java
@@ -67,6 +67,6 @@ public class AsyncProducerExample {
         Thread.sleep(Long.MAX_VALUE);
         // Close the producer when you don't need it anymore.
         // You could close it manually or add this into the JVM shutdown hook.
-        // producer.shutdown();
+        // producer.close();
     }
 }
