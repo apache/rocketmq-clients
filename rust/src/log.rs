d94b1f63437b215320ade09787c28d9e2e85466a
fuyoo <fyaiym@qq.com>
2024-04-26 10:55:33 +0800

    修复当使用json文件日志的时候，panic的问题 1.可以自定义配置文件路径 2.当配置文件创建失败时，退回到输出日志到stdout

--- rust/src/log.rs
+++ rust/src/log.rs
@@ -14,7 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-use std::fs::OpenOptions;
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::path::{PathBuf};
 
 use slog::{o, Drain, Logger};
 use slog_async::OverflowStrategy;
@@ -35,21 +37,102 @@ pub(crate) fn terminal_logger() -> Logger {
     Logger::root(drain, o!())
 }
 
-fn json_logger(filepath: &str) -> Logger {
-    let file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .truncate(true)
-        .open(filepath)
-        .unwrap();
-    let decorator = slog_json::Json::default(file).fuse();
-    let drain = slog_async::Async::new(decorator).build().fuse();
-    Logger::root(drain, o!())
+fn json_logger(filepath: &Option<String>) -> Logger {
+    // 设置默认值和解析传入的参数
+    let fp = match filepath {
+        None => {
+            PathBuf::from("logs/rocketmq.client.log")
+        }
+        Some(p) => {
+            PathBuf::from(p)
+        }
+    };
+    // first check the director or file is existing?
+    let file = if !fp.exists() {
+        let create_file = |fp: &PathBuf| -> Option<File> {
+            match OpenOptions::new()
+                .create(true)
+                .write(true)
+                .append(true)
+                .open(fp) {
+                Ok(f) => Some(f),
+                Err(_) => None
+            }
+        };
+        // have the parent dir
+        match fp.parent() {
+            // or no have
+            None => create_file(&fp),
+            // have the parent dir, create it, if success create the file.
+            Some(p) => match p.exists() {
+                true => create_file(&fp),
+                false => match fs::create_dir(p) {
+                    Ok(_) => create_file(&fp),
+                    // failed to return None
+                    Err(_) => None
+                }
+            }
+        }
+    } else {
+        // log file is existed created it.
+        match OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(fp) {
+            Ok(f) => Some(f),
+            Err(_) => None
+        }
+    };
+
+    match file {
+        None => terminal_logger(),
+        Some(f) => {
+            let decorator = slog_json::Json::default(f).fuse();
+            let drain = slog_async::Async::new(decorator).build().fuse();
+            Logger::root(drain, o!())
+        }
+    }
 }
 
 pub(crate) fn logger(logging_format: &LoggingFormat) -> Logger {
     match logging_format {
         LoggingFormat::Terminal => terminal_logger(),
-        LoggingFormat::Json => json_logger("logs/rocketmq_client.log"),
+        LoggingFormat::Json(p) => json_logger(p),
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use std::path::PathBuf;
+    use crate::conf::LoggingFormat;
+    use crate::log::logger;
+
+    #[test]
+    pub fn create_default_log_file() {
+        let _ = logger(&LoggingFormat::Json(None));
+        let p = PathBuf::from("logs/rocketmq.client.log");
+        assert_eq!(p.exists(), true);
+    }
+
+    #[test]
+    pub fn create_custom_log_file() {
+        let _ = logger(&LoggingFormat::Json(Some(String::from("log/log.log"))));
+        let p = PathBuf::from("log/log.log");
+        assert_eq!(p.exists(), true);
+    }
+
+    #[test]
+    pub fn create_custom_log_no_parent_dir_failed() {
+        let _ = logger(&LoggingFormat::Json(Some(String::from("log.log"))));
+        let p = PathBuf::from("log.log");
+        assert_eq!(p.exists(), false);
+    }
+
+    #[test]
+    pub fn create_custom_log_no_parent_dir() {
+        let _ = logger(&LoggingFormat::Json(Some(String::from("./log1.log"))));
+        let p = PathBuf::from("./log1.log");
+        assert_eq!(p.exists(), true);
     }
 }
