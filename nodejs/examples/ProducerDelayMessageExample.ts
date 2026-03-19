5b54c566f4b5eebc1eb6ac69b17ba75360189eb9
zhaohai <33314633+zhaohai666@users.noreply.github.com>
2026-03-19 15:54:27 +0800

    nodeJs demo (#975)
    * add demo
    
    * update async
    
    * update TransactionResolution import
    
    * remove key
    
    * fix Removing old Proto Files
    
    ---------
    
    Co-authored-by: zh378814 <wb-zh378814@alibaba-inc.com>

--- nodejs/examples/ProducerDelayMessageExample.ts
+++ nodejs/examples/ProducerDelayMessageExample.ts
@@ -15,27 +15,27 @@
  * limitations under the License.
  */
 
-import { SimpleConsumer } from '..';
-
-import { topics, endpoints, sessionCredentials, namespace, tag, consumerGroup } from './ProducerSingleton';
-
+import { Producer } from '..';
+import { topics, endpoints, sessionCredentials, namespace } from './ProducerSingleton';
 
 (async () => {
-  const simpleConsumer = new SimpleConsumer({
-    consumerGroup,
+  const producer = new Producer({
     endpoints,
     namespace,
     sessionCredentials,
-    subscriptions: new Map().set(topics.normal, tag),
-    awaitDuration: 3000,
+    maxAttempts: 2,
   });
-  await simpleConsumer.startup();
+  await producer.startup();
 
-  const messages = await simpleConsumer.receive(20);
-  console.log('got %d messages', messages.length);
-  for (const message of messages) {
-    console.log(message);
-    console.log('body=%o', message.body.toString());
-    await simpleConsumer.ack(message);
-  }
+  const receipt = await producer.send({
+    topic: topics.delay,
+    tag: 'nodejs-delay',
+    delay: 2000,
+    body: Buffer.from(JSON.stringify({
+      hello: 'rocketmq-client-nodejs world 😄',
+      now: Date(),
+    })),
+  });
+  console.log(receipt);
 })();
+
