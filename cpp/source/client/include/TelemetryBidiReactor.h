94f64b8e0f12c3ed4b6d88c712bbd797d3ce8d2c
lizhimins <707364882@qq.com>
2026-07-09 20:15:51 +0800

    [C++] Fix thread safety bugs in TelemetryBidiReactor and ClientManager

--- cpp/source/client/include/TelemetryBidiReactor.h
+++ cpp/source/client/include/TelemetryBidiReactor.h
@@ -25,6 +25,7 @@
 #include <utility>
 #include <vector>
 
+#include "absl/container/flat_hash_map.h"
 #include "Client.h"
 #include "RpcClient.h"
 #include "absl/base/thread_annotations.h"
@@ -131,6 +132,10 @@ private:
   absl::CondVar state_cv_;
 
   std::promise<bool> sync_settings_promise_;
+  std::atomic<bool> settings_received_{false};
+
+  /// Set to true when close() is called intentionally. Suppresses reconnection in OnDone().
+  std::atomic<bool> intentional_close_{false};
 
   void applySettings(const rmq::Settings& settings);
 
