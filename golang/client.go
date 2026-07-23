apache/rocketmq-clients

commit bbcbcac196ff46bc7f857251ee117d3f7484f959

feat: Make each 'Client' have an independent 'ClientManager' (#235)
* feat: Make each 'Client' have an independent 'ClientManager'

* remove clientManagerRegistry

golang/client.go
@@ -418,7 +418,10 @@ func (cli *defaultClient) telemeter(target string, command *v2.TelemetryCommand)
 
 func (cli *defaultClient) startUp() error {
 	cli.log.Infof("begin to start the rocketmq client")
-	cli.clientManager = defaultClientManagerRegistry.RegisterClient(cli)
+	cm := NewDefaultClientManager()
+	cm.startUp()
+	cm.RegisterClient(cli)
+	cli.clientManager = cm
 	for _, topic := range cli.initTopics {
 		maxAttempts := int(cli.settings.GetRetryPolicy().GetMaxAttempts())
 		for i := 0; i < maxAttempts; i++ {
@@ -486,7 +489,7 @@ func (cli *defaultClient) GracefulStop() error {
 		return fmt.Errorf("client has been closed")
 	}
 	cli.notifyClientTermination()
-	defaultClientManagerRegistry.UnRegisterClient(cli)
+	cli.clientManager.UnRegisterClient(cli)
 	cli.done <- struct{}{}
 	close(cli.done)
 	cli.clientMeterProvider.Reset(&v2.Metric{

golang/client_manager.go
@@ -84,55 +84,6 @@ type defaultClientManager struct {
 	opts               clientManagerOptions
 }
 
-type ClientManagerRegistry interface {
-	RegisterClient(client Client) ClientManager
-	UnRegisterClient(client Client) bool
-}
-
-type clientManagerRegistry struct {
-	clientIds              map[string]bool
-	clientIdsLock          sync.Mutex
-	singletonClientManager ClientManager
-}
-
-var defaultClientManagerRegistry = &clientManagerRegistry{
-	clientIds: make(map[string]bool),
-}
-
-var _ = ClientManagerRegistry(&clientManagerRegistry{})
-
-func (cmr *clientManagerRegistry) RegisterClient(client Client) ClientManager {
-	cmr.clientIdsLock.Lock()
-	defer cmr.clientIdsLock.Unlock()
-
-	if cmr.singletonClientManager == nil {
-		cmr.singletonClientManager = NewDefaultClientManager()
-		cmr.singletonClientManager.(*defaultClientManager).startUp()
-	}
-	cmr.clientIds[client.GetClientID()] = true
-	cmr.singletonClientManager.RegisterClient(client)
-	return cmr.singletonClientManager
-}
-
-func (cmr *clientManagerRegistry) UnRegisterClient(client Client) bool {
-	var tmpClientManager ClientManager
-
-	cmr.clientIdsLock.Lock()
-	{
-		delete(cmr.clientIds, client.GetClientID())
-		cmr.singletonClientManager.UnRegisterClient(client)
-		if len(cmr.clientIds) == 0 {
-			tmpClientManager = cmr.singletonClientManager
-			cmr.singletonClientManager = nil
-		}
-	}
-	cmr.clientIdsLock.Unlock()
-	if tmpClientManager != nil {
-		tmpClientManager.(*defaultClientManager).shutdown()
-	}
-	return tmpClientManager != nil
-}
-
 var _ = ClientManager(&defaultClientManager{})
 
 var NewDefaultClientManager = func() *defaultClientManager {

golang/client_manager_mock.go
@@ -207,54 +207,3 @@ func (mr *MockClientManagerMockRecorder) UnRegisterClient(client interface{}) *g
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UnRegisterClient", reflect.TypeOf((*MockClientManager)(nil).UnRegisterClient), client)
 }
-
-// MockClientManagerRegistry is a mock of ClientManagerRegistry interface.
-type MockClientManagerRegistry struct {
-	ctrl     *gomock.Controller
-	recorder *MockClientManagerRegistryMockRecorder
-}
-
-// MockClientManagerRegistryMockRecorder is the mock recorder for MockClientManagerRegistry.
-type MockClientManagerRegistryMockRecorder struct {
-	mock *MockClientManagerRegistry
-}
-
-// NewMockClientManagerRegistry creates a new mock instance.
-func NewMockClientManagerRegistry(ctrl *gomock.Controller) *MockClientManagerRegistry {
-	mock := &MockClientManagerRegistry{ctrl: ctrl}
-	mock.recorder = &MockClientManagerRegistryMockRecorder{mock}
-	return mock
-}
-
-// EXPECT returns an object that allows the caller to indicate expected use.
-func (m *MockClientManagerRegistry) EXPECT() *MockClientManagerRegistryMockRecorder {
-	return m.recorder
-}
-
-// RegisterClient mocks base method.
-func (m *MockClientManagerRegistry) RegisterClient(client Client) ClientManager {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "RegisterClient", client)
-	ret0, _ := ret[0].(ClientManager)
-	return ret0
-}
-
-// RegisterClient indicates an expected call of RegisterClient.
-func (mr *MockClientManagerRegistryMockRecorder) RegisterClient(client interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RegisterClient", reflect.TypeOf((*MockClientManagerRegistry)(nil).RegisterClient), client)
-}
-
-// UnRegisterClient mocks base method.
-func (m *MockClientManagerRegistry) UnRegisterClient(client Client) bool {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "UnRegisterClient", client)
-	ret0, _ := ret[0].(bool)
-	return ret0
-}
-
-// UnRegisterClient indicates an expected call of UnRegisterClient.
-func (mr *MockClientManagerRegistryMockRecorder) UnRegisterClient(client interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UnRegisterClient", reflect.TypeOf((*MockClientManagerRegistry)(nil).UnRegisterClient), client)
-}

golang/client_manager_test.go
@@ -121,10 +121,12 @@ func TestMain(m *testing.M) {
 	m.Run()
 	sugarBaseLogger.Info("end")
 }
-
 func TestCMRegisterClient(t *testing.T) {
-	defaultClientManagerRegistry.RegisterClient(MOCK_CLIENT)
-	v, ok := defaultClientManagerRegistry.singletonClientManager.(*defaultClientManager).clientTable.Load(MOCK_CLIENT_ID)
+	cm := NewDefaultClientManager()
+	cm.startUp()
+	cm.RegisterClient(MOCK_CLIENT)
+	defer cm.UnRegisterClient(MOCK_CLIENT)
+	v, ok := cm.clientTable.Load(MOCK_CLIENT_ID)
 	if !ok {
 		t.Errorf("test RegisterClient failed")
 	}
@@ -138,9 +140,11 @@ func TestCMRegisterClient(t *testing.T) {
 }
 
 func TestCMUnRegisterClient(t *testing.T) {
-	defaultClientManagerRegistry.RegisterClient(MOCK_CLIENT)
-	defaultClientManagerRegistry.UnRegisterClient(MOCK_CLIENT)
-	if defaultClientManagerRegistry.singletonClientManager != nil {
+	cm := NewDefaultClientManager()
+	cm.startUp()
+	cm.RegisterClient(MOCK_CLIENT)
+	defer cm.UnRegisterClient(MOCK_CLIENT)
+	if _, ok := cm.clientTable.Load(MOCK_CLIENT.GetClientID()); !ok {
 		t.Errorf("test UnRegisterClient failed")
 	}
 }
@@ -162,8 +166,10 @@ func fakeEndpoints() *v2.Endpoints {
 	}
 }
 func TestCMQueryRoute(t *testing.T) {
-	cm := defaultClientManagerRegistry.RegisterClient(MOCK_CLIENT)
-	defer defaultClientManagerRegistry.UnRegisterClient(MOCK_CLIENT)
+	cm := NewDefaultClientManager()
+	cm.startUp()
+	cm.RegisterClient(MOCK_CLIENT)
+	defer cm.UnRegisterClient(MOCK_CLIENT)
 
 	MOCK_RPC_CLIENT.EXPECT().QueryRoute(gomock.Any(), gomock.Any()).Return(&v2.QueryRouteResponse{
 		Status: &v2.Status{
@@ -180,8 +186,10 @@ func TestCMQueryRoute(t *testing.T) {
 }
 
 func TestCMHeartBeat(t *testing.T) {
-	cm := defaultClientManagerRegistry.RegisterClient(MOCK_CLIENT)
-	defer defaultClientManagerRegistry.UnRegisterClient(MOCK_CLIENT)
+	cm := NewDefaultClientManager()
+	cm.startUp()
+	cm.RegisterClient(MOCK_CLIENT)
+	defer cm.UnRegisterClient(MOCK_CLIENT)
 
 	resp, err := cm.HeartBeat(context.TODO(), fakeEndpoints(), &v2.HeartbeatRequest{}, time.Minute)
 	if err != nil {
@@ -193,8 +201,10 @@ func TestCMHeartBeat(t *testing.T) {
 }
 
 func TestCMSendMessage(t *testing.T) {
-	cm := defaultClientManagerRegistry.RegisterClient(MOCK_CLIENT)
-	defer defaultClientManagerRegistry.UnRegisterClient(MOCK_CLIENT)
+	cm := NewDefaultClientManager()
+	cm.startUp()
+	cm.RegisterClient(MOCK_CLIENT)
+	defer cm.UnRegisterClient(MOCK_CLIENT)
 
 	MOCK_RPC_CLIENT.EXPECT().SendMessage(gomock.Any(), gomock.Any()).Return(&v2.SendMessageResponse{
 		Status: &v2.Status{
@@ -211,8 +221,10 @@ func TestCMSendMessage(t *testing.T) {
 }
 
 func TestCMTelemetry(t *testing.T) {
-	cm := defaultClientManagerRegistry.RegisterClient(MOCK_CLIENT)
-	defer defaultClientManagerRegistry.UnRegisterClient(MOCK_CLIENT)
+	cm := NewDefaultClientManager()
+	cm.startUp()
+	cm.RegisterClient(MOCK_CLIENT)
+	defer cm.UnRegisterClient(MOCK_CLIENT)
 
 	MOCK_RPC_CLIENT.EXPECT().Telemetry(gomock.Any()).Return(nil, nil)
 	_, err := cm.Telemetry(context.TODO(), fakeEndpoints(), time.Minute)
@@ -222,8 +234,10 @@ func TestCMTelemetry(t *testing.T) {
 }
 
 func TestCMEndTransaction(t *testing.T) {
-	cm := defaultClientManagerRegistry.RegisterClient(MOCK_CLIENT)
-	defer defaultClientManagerRegistry.UnRegisterClient(MOCK_CLIENT)
+	cm := NewDefaultClientManager()
+	cm.startUp()
+	cm.RegisterClient(MOCK_CLIENT)
+	defer cm.UnRegisterClient(MOCK_CLIENT)
 
 	MOCK_RPC_CLIENT.EXPECT().EndTransaction(gomock.Any(), gomock.Any()).Return(&v2.EndTransactionResponse{
 		Status: &v2.Status{
@@ -240,8 +254,10 @@ func TestCMEndTransaction(t *testing.T) {
 }
 
 func TestCMNotifyClientTermination(t *testing.T) {
-	cm := defaultClientManagerRegistry.RegisterClient(MOCK_CLIENT)
-	defer defaultClientManagerRegistry.UnRegisterClient(MOCK_CLIENT)
+	cm := NewDefaultClientManager()
+	cm.startUp()
+	cm.RegisterClient(MOCK_CLIENT)
+	defer cm.UnRegisterClient(MOCK_CLIENT)
 
 	MOCK_RPC_CLIENT.EXPECT().NotifyClientTermination(gomock.Any(), gomock.Any()).Return(&v2.NotifyClientTerminationResponse{
 		Status: &v2.Status{
@@ -258,8 +274,10 @@ func TestCMNotifyClientTermination(t *testing.T) {
 }
 
 func TestCMReceiveMessage(t *testing.T) {
-	cm := defaultClientManagerRegistry.RegisterClient(MOCK_CLIENT)
-	defer defaultClientManagerRegistry.UnRegisterClient(MOCK_CLIENT)
+	cm := NewDefaultClientManager()
+	cm.startUp()
+	cm.RegisterClient(MOCK_CLIENT)
+	defer cm.UnRegisterClient(MOCK_CLIENT)
 
 	MOCK_RPC_CLIENT.EXPECT().ReceiveMessage(gomock.Any(), gomock.Any()).Return(nil, nil)
 	_, err := cm.ReceiveMessage(context.TODO(), fakeEndpoints(), &v2.ReceiveMessageRequest{})
@@ -269,8 +287,10 @@ func TestCMReceiveMessage(t *testing.T) {
 }
 
 func TestCMAckMessage(t *testing.T) {
-	cm := defaultClientManagerRegistry.RegisterClient(MOCK_CLIENT)
-	defer defaultClientManagerRegistry.UnRegisterClient(MOCK_CLIENT)
+	cm := NewDefaultClientManager()
+	cm.startUp()
+	cm.RegisterClient(MOCK_CLIENT)
+	defer cm.UnRegisterClient(MOCK_CLIENT)
 
 	MOCK_RPC_CLIENT.EXPECT().AckMessage(gomock.Any(), gomock.Any()).Return(&v2.AckMessageResponse{
 		Status: &v2.Status{
@@ -305,13 +325,16 @@ func TestCMClearIdleRpcClients(t *testing.T) {
 	defer stubs.Reset()
 
 	MOCK_RPC_CLIENT.EXPECT().idleDuration().Return(time.Hour * 24 * 365)
-	cm := defaultClientManagerRegistry.RegisterClient(MOCK_CLIENT)
-	defer defaultClientManagerRegistry.UnRegisterClient(MOCK_CLIENT)
+	cm := NewDefaultClientManager()
+	cm.startUp()
+	cm.RegisterClient(MOCK_CLIENT)
+	defer cm.UnRegisterClient(MOCK_CLIENT)
 
 	cm.HeartBeat(context.TODO(), fakeEndpoints(), &v2.HeartbeatRequest{}, time.Minute)
 
 	startTime := time.Now()
-	for len(defaultClientManagerRegistry.singletonClientManager.(*defaultClientManager).rpcClientTable) != 0 {
+
+	for len(cm.rpcClientTable) != 0 {
 		if time.Since(startTime) > time.Second*5 {
 			t.Errorf("test ClearIdleRpcClients failed")
 		}

commit d144b1f2b1199023780d1f28ac58fa6b5d9276a2

[Go] Fix resource leaks on client close
1. ClientManager shutdown: call shutdown() instead of UnRegisterClient()
   in GracefulStop to stop background goroutines and close RPC connections
   (regression from #235 which made ClientManager per-client but forgot
   to add shutdown call).

2. Telemetry session cleanup: release all sessions in GracefulStop before
   shutting down the client manager to properly close gRPC streams.

3. release() nil check: guard against nil observer when telemetry
   connection failed during startup.

4. Per-client metrics isolation: use view.NewMeter() to give each client
   its own Meter, Measures, Views, and Exporter. On client close, the
   Meter is stopped and views are unregistered, preventing metrics
   cardinality explosion from accumulated client_id tag values.
   The client_id tag is preserved in exported metric data.

5. Fix meter lifecycle: call Start() before Register() to avoid deadlock,
   and register/unregister the exporter with the per-client Meter so
   metrics are actually exported.

golang/client.go
@@ -181,10 +181,12 @@ func (cs *defaultClientSession) handleTelemetryCommand(response *v2.TelemetryCom
 func (cs *defaultClientSession) release() {
 	cs.observerLock.Lock()
 	defer cs.observerLock.Unlock()
-	if err := cs.observer.CloseSend(); err != nil {
-		cs.cli.log.Errorf("release defaultClientSession err=%v", err)
+	if cs.observer != nil {
+		if err := cs.observer.CloseSend(); err != nil {
+			cs.cli.log.Errorf("release defaultClientSession err=%v", err)
+		}
+		cs.observer = nil
 	}
-	cs.observer = nil
 }
 func (cs *defaultClientSession) publish(ctx context.Context, common *v2.TelemetryCommand) error {
 	var err error
@@ -646,8 +648,14 @@ func (cli *defaultClient) GracefulStop() error {
 		return fmt.Errorf("client has been closed")
 	}
 	cli.notifyClientTermination()
+	cli.endpointsTelemetryClientsLock.Lock()
+	for _, session := range cli.endpointsTelemetryClientTable {
+		session.release()
+	}
+	cli.endpointsTelemetryClientTable = make(map[string]*defaultClientSession)
+	cli.endpointsTelemetryClientsLock.Unlock()
 	if cli.clientManager != nil {
-		cli.clientManager.UnRegisterClient(cli)
+		cli.clientManager.shutdown()
 	}
 	cli.done <- struct{}{}
 	close(cli.done)

golang/client_manager.go
@@ -46,6 +46,7 @@ type ClientManager interface {
 	ForwardMessageToDeadLetterQueue(ctx context.Context, endpoints *v2.Endpoints, request *v2.ForwardMessageToDeadLetterQueueRequest, duration time.Duration) (*v2.ForwardMessageToDeadLetterQueueResponse, error)
 	SyncLiteSubscription(ctx context.Context, endpoints *v2.Endpoints, request *v2.SyncLiteSubscriptionRequest, duration time.Duration) (*v2.SyncLiteSubscriptionResponse, error)
 	RecallMessage(ctx context.Context, endpoints *v2.Endpoints, request *v2.RecallMessageRequest, duration time.Duration) (*v2.RecallMessageResponse, error)
+	shutdown()
 }
 
 type clientManagerOptions struct {

golang/client_manager_mock.go
@@ -268,3 +268,15 @@ func (mr *MockClientManagerMockRecorder) UnRegisterClient(client interface{}) *g
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UnRegisterClient", reflect.TypeOf((*MockClientManager)(nil).UnRegisterClient), client)
 }
+
+// shutdown mocks base method.
+func (m *MockClientManager) shutdown() {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "shutdown")
+}
+
+// shutdown indicates an expected call of shutdown.
+func (mr *MockClientManagerMockRecorder) shutdown() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "shutdown", reflect.TypeOf((*MockClientManager)(nil).shutdown))
+}

golang/metric.go
@@ -19,6 +19,7 @@ package golang
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"time"
 
@@ -46,57 +47,113 @@ var (
 	clientIdTag, _         = tag.NewKey("client_id")
 	invocationStatusTag, _ = tag.NewKey("invocation_status")
 	consumerGroupTag, _    = tag.NewKey("consumer_group")
-
-	PublishMLatencyMs         = stats.Int64("publish_latency", "Publish latency in milliseconds", "ms")
-	ConsumeDeliveryMLatencyMs = stats.Int64("delivery_latency", "Time spent delivering messages from servers to clients", "ms")
-	ConsumeAwaitMLatencyMs    = stats.Int64("await_time", "Client side queuing time of messages before getting processed", "ms")
-	ConsumeProcessMLatencyMs  = stats.Int64("process_time", "Process message time", "ms")
-
-	PublishLatencyView = view.View{
-		Name:        "rocketmq_send_cost_time",
-		Description: "Publish latency",
-		Measure:     PublishMLatencyMs,
-		Aggregation: view.Distribution(1, 5, 10, 20, 50, 200, 500),
-		TagKeys:     []tag.Key{topicTag, clientIdTag, invocationStatusTag},
-	}
-
-	ConsumeDeliveryLatencyView = view.View{
-		Name:        "rocketmq_delivery_latency",
-		Description: "Message delivery latency",
-		Measure:     ConsumeDeliveryMLatencyMs,
-		Aggregation: view.Distribution(1, 5, 10, 20, 50, 200, 500),
-		TagKeys:     []tag.Key{topicTag, clientIdTag, consumerGroupTag},
-	}
-
-	ConsumeAwaitTimeView = view.View{
-		Name:        "rocketmq_await_time",
-		Description: "Message await time",
-		Measure:     ConsumeAwaitMLatencyMs,
-		Aggregation: view.Distribution(1, 5, 20, 100, 1000, 5000, 10000),
-		TagKeys:     []tag.Key{topicTag, clientIdTag, consumerGroupTag},
-	}
-
-	ConsumeProcessTimeView = view.View{
-		Name:        "rocketmq_process_time",
-		Description: "Message process time",
-		Measure:     ConsumeProcessMLatencyMs,
-		Aggregation: view.Distribution(1, 5, 10, 100, 1000, 10000, 60000),
-		TagKeys:     []tag.Key{topicTag, clientIdTag, consumerGroupTag, invocationStatusTag},
-	}
 )
 
-func init() {
-	if err := view.Register(&PublishLatencyView, &ConsumeDeliveryLatencyView, &ConsumeAwaitTimeView, &ConsumeProcessTimeView); err != nil {
-		sugarBaseLogger.Fatalf("failed to register views: %v", err)
-	}
-	view.SetReportingPeriod(time.Minute)
-}
+type meterType int
+
+const (
+	meterPublishLatency meterType = iota
+	meterDeliveryLatency
+	meterAwaitTime
+	meterProcessTime
+)
 
 type defaultClientMeter struct {
 	enabled     atomic.Bool
 	endpoints   *v2.Endpoints
 	ocaExporter view.Exporter
 	mutex       sync.Mutex
+
+	meter           view.Meter
+	publishMeasure  *stats.Int64Measure
+	deliveryMeasure *stats.Int64Measure
+	awaitMeasure    *stats.Int64Measure
+	processMeasure  *stats.Int64Measure
+	registeredViews []*view.View
+}
+
+func newDefaultClientMeter(enabled bool, exporter view.Exporter, endpoints *v2.Endpoints, clientID string) *defaultClientMeter {
+	dcm := &defaultClientMeter{
+		enabled:     *atomic.NewBool(enabled),
+		endpoints:   endpoints,
+		ocaExporter: exporter,
+	}
+	if enabled {
+		dcm.initPerClientResources(clientID)
+	}
+	return dcm
+}
+
+func (dcm *defaultClientMeter) initPerClientResources(clientID string) {
+	dcm.meter = view.NewMeter()
+
+	prefix := fmt.Sprintf("rocketmq_%s", clientID)
+	dcm.publishMeasure = stats.Int64(prefix+"_publish_latency", "Publish latency in milliseconds", "ms")
+	dcm.deliveryMeasure = stats.Int64(prefix+"_delivery_latency", "Time spent delivering messages from servers to clients", "ms")
+	dcm.awaitMeasure = stats.Int64(prefix+"_await_time", "Client side queuing time of messages before getting processed", "ms")
+	dcm.processMeasure = stats.Int64(prefix+"_process_time", "Process message time", "ms")
+
+	dcm.registeredViews = []*view.View{
+		{
+			Name:        "rocketmq_send_cost_time",
+			Description: "Publish latency",
+			Measure:     dcm.publishMeasure,
+			Aggregation: view.Distribution(1, 5, 10, 20, 50, 200, 500),
+			TagKeys:     []tag.Key{topicTag, clientIdTag, invocationStatusTag},
+		},
+		{
+			Name:        "rocketmq_delivery_latency",
+			Description: "Message delivery latency",
+			Measure:     dcm.deliveryMeasure,
+			Aggregation: view.Distribution(1, 5, 10, 20, 50, 200, 500),
+			TagKeys:     []tag.Key{topicTag, clientIdTag, consumerGroupTag},
+		},
+		{
+			Name:        "rocketmq_await_time",
+			Description: "Message await time",
+			Measure:     dcm.awaitMeasure,
+			Aggregation: view.Distribution(1, 5, 20, 100, 1000, 5000, 10000),
+			TagKeys:     []tag.Key{topicTag, clientIdTag, consumerGroupTag},
+		},
+		{
+			Name:        "rocketmq_process_time",
+			Description: "Message process time",
+			Measure:     dcm.processMeasure,
+			Aggregation: view.Distribution(1, 5, 10, 100, 1000, 10000, 60000),
+			TagKeys:     []tag.Key{topicTag, clientIdTag, consumerGroupTag, invocationStatusTag},
+		},
+	}
+
+	dcm.meter.Start()
+	if err := dcm.meter.Register(dcm.registeredViews...); err != nil {
+		sugarBaseLogger.Errorf("failed to register per-client views: %v", err)
+	}
+}
+
+func (dcm *defaultClientMeter) record(mt meterType, mutators []tag.Mutator, val int64) {
+	if !dcm.enabled.Load() || dcm.meter == nil {
+		return
+	}
+	var measure *stats.Int64Measure
+	switch mt {
+	case meterPublishLatency:
+		measure = dcm.publishMeasure
+	case meterDeliveryLatency:
+		measure = dcm.deliveryMeasure
+	case meterAwaitTime:
+		measure = dcm.awaitMeasure
+	case meterProcessTime:
+		measure = dcm.processMeasure
+	default:
+		return
+	}
+	ctx, err := tag.New(context.Background(), mutators...)
+	if err != nil {
+		sugarBaseLogger.Errorf("failed to create tag map: %v", err)
+		return
+	}
+	tagMap := tag.FromContext(ctx)
+	dcm.meter.Record(tagMap, []stats.Measurement{measure.M(val)}, nil)
 }
 
 func (dcm *defaultClientMeter) shutdown() {
@@ -105,31 +162,38 @@ func (dcm *defaultClientMeter) shutdown() {
 	}
 	dcm.mutex.Lock()
 	defer dcm.mutex.Unlock()
-	view.UnregisterExporter(dcm.ocaExporter)
+
+	if dcm.meter != nil {
+		if dcm.ocaExporter != nil {
+			dcm.meter.UnregisterExporter(dcm.ocaExporter)
+		}
+		dcm.meter.Unregister(dcm.registeredViews...)
+		dcm.meter.Stop()
+		dcm.meter = nil
+	}
+
 	if dcm.ocaExporter != nil {
-		exporter, ok := dcm.ocaExporter.(*ocagent.Exporter)
-		if ok {
-			err := exporter.Stop()
-			if err != nil {
+		if exporter, ok := dcm.ocaExporter.(*ocagent.Exporter); ok {
+			if err := exporter.Stop(); err != nil {
 				sugarBaseLogger.Errorf("ocExporter stop failed, err=%w", err)
 			}
 		}
+		dcm.ocaExporter = nil
 	}
+	dcm.enabled.Store(false)
 }
 
 func (dcm *defaultClientMeter) start() {
 	if !dcm.enabled.Load() {
 		return
 	}
-	view.RegisterExporter(dcm.ocaExporter)
+	if dcm.meter != nil && dcm.ocaExporter != nil {
+		dcm.meter.RegisterExporter(dcm.ocaExporter)
+	}
 }
 
 var NewDefaultClientMeter = func(exporter view.Exporter, on bool, endpoints *v2.Endpoints, clientID string) *defaultClientMeter {
-	return &defaultClientMeter{
-		enabled:     *atomic.NewBool(on),
-		endpoints:   endpoints,
-		ocaExporter: exporter,
-	}
+	return newDefaultClientMeter(on, exporter, endpoints, clientID)
 }
 
 type MessageMeterInterceptor interface {
@@ -145,6 +209,7 @@ type ClientMeterProvider interface {
 	isEnabled() bool
 	getClientID() string
 	getClientImpl() isClient
+	record(mt meterType, tags []tag.Mutator, val int64)
 }
 
 var _ = ClientMeterProvider(&defaultClientMeterProvider{})
@@ -153,6 +218,10 @@ type defaultClientMeterProvider struct {
 	client      Client
 	clientMeter *defaultClientMeter
 	globalMutex sync.Mutex
+}
+
+func (dcmp *defaultClientMeterProvider) record(mt meterType, tags []tag.Mutator, val int64) {
+	dcmp.clientMeter.record(mt, tags, val)
 }
 
 func (dcmp *defaultClientMeterProvider) getClientImpl() isClient {
@@ -195,10 +264,9 @@ func (dmmi *defaultMessageMeterInterceptor) doBeforeConsumeMessage(messageCommon
 			continue
 		}
 		duration := time.Since(*messageCommon.decodeStopwatch)
-		err := stats.RecordWithTags(context.Background(), []tag.Mutator{tag.Insert(topicTag, messageCommon.topic), tag.Insert(clientIdTag, dmmi.clientMeterProvider.getClientID()), tag.Insert(consumerGroupTag, consumerGroup)}, ConsumeAwaitMLatencyMs.M(duration.Milliseconds()))
-		if err != nil {
-			return err
-		}
+		dmmi.clientMeterProvider.record(meterAwaitTime,
+			[]tag.Mutator{tag.Insert(topicTag, messageCommon.topic), tag.Insert(clientIdTag, clientId), tag.Insert(consumerGroupTag, consumerGroup)},
+			duration.Milliseconds())
 	}
 
 	return nil
@@ -230,10 +298,9 @@ func (dmmi *defaultMessageMeterInterceptor) doAfterConsumeMessage(messageCommons
 		invocationStatus = InvocationStatus_SUCCESS
 	}
 	for _, messageCommon := range messageCommons {
-		err := stats.RecordWithTags(context.Background(), []tag.Mutator{tag.Insert(topicTag, messageCommon.topic), tag.Insert(clientIdTag, dmmi.clientMeterProvider.getClientID()), tag.Insert(consumerGroupTag, consumerGroup), tag.Insert(invocationStatusTag, string(invocationStatus))}, ConsumeProcessMLatencyMs.M(duration.Milliseconds()))
-		if err != nil {
-			return err
-		}
+		dmmi.clientMeterProvider.record(meterProcessTime,
+			[]tag.Mutator{tag.Insert(topicTag, messageCommon.topic), tag.Insert(clientIdTag, clientId), tag.Insert(consumerGroupTag, consumerGroup), tag.Insert(invocationStatusTag, string(invocationStatus))},
+			duration.Milliseconds())
 	}
 
 	return nil
@@ -265,10 +332,9 @@ func (dmmi *defaultMessageMeterInterceptor) doAfterReceiveMessage(messageCommons
 			continue
 		}
 		latency := time.Since(*messageCommon.deliveryTimestamp)
-		err := stats.RecordWithTags(context.Background(), []tag.Mutator{tag.Insert(topicTag, messageCommon.topic), tag.Insert(clientIdTag, dmmi.clientMeterProvider.getClientID()), tag.Insert(consumerGroupTag, consumerGroup)}, ConsumeDeliveryMLatencyMs.M(latency.Milliseconds()))
-		if err != nil {
-			return err
-		}
+		dmmi.clientMeterProvider.record(meterDeliveryLatency,
+			[]tag.Mutator{tag.Insert(topicTag, messageCommon.topic), tag.Insert(clientIdTag, clientId), tag.Insert(consumerGroupTag, consumerGroup)},
+			latency.Milliseconds())
 	}
 
 	return nil
@@ -292,11 +358,11 @@ func (dmmi *defaultMessageMeterInterceptor) doAfterSendMessage(messageCommons []
 	if status == MessageHookPointsStatus_OK {
 		invocationStatus = InvocationStatus_SUCCESS
 	}
+	clientId := dmmi.clientMeterProvider.getClientID()
 	for _, messageCommon := range messageCommons {
-		err := stats.RecordWithTags(context.Background(), []tag.Mutator{tag.Insert(topicTag, messageCommon.topic), tag.Insert(clientIdTag, dmmi.clientMeterProvider.getClientID()), tag.Insert(invocationStatusTag, string(invocationStatus))}, PublishMLatencyMs.M(duration.Milliseconds()))
-		if err != nil {
-			return err
-		}
+		dmmi.clientMeterProvider.record(meterPublishLatency,
+			[]tag.Mutator{tag.Insert(topicTag, messageCommon.topic), tag.Insert(clientIdTag, clientId), tag.Insert(invocationStatusTag, string(invocationStatus))},
+			duration.Milliseconds())
 	}
 	return nil
 }

golang/lite_push_consumer_test.go
@@ -181,6 +181,8 @@ func (m *mockedClientManager) RecallMessage(ctx context.Context, endpoints *v2.E
 	return nil, nil
 }
 
+func (m *mockedClientManager) shutdown() {}
+
 func TestLitePushConsumer_SubscribeLite(t *testing.T) {
 	setupTest(t)
 	defer teardownTest()